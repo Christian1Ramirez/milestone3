947c8f05990e1689392e75984ac7b8bea79393db
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-09-14 21:10:51 -0700

    added edit function to tours and made code more DRY

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -2,6 +2,30 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import supabase from '../services/supabaseClient';
 
+function CustomInput({ placeholder, type, value, onChange }) {
+  return <input type={type} placeholder={placeholder} value={value} onChange={onChange} />;
+}
+
+function CustomCheckbox({ label, checked, onChange }) {
+  return (
+    <label>
+      <input type="checkbox" checked={checked} onChange={onChange} />
+      {label}
+    </label>
+  );
+}
+
+function CustomSelect({ value, options, onChange }) {
+  return (
+    <select value={value} onChange={onChange}>
+      <option value="" disabled>Premium</option>
+      {options.map((option, index) => (
+        <option key={index} value={option}>{option}</option>
+      ))}
+    </select>
+  );
+}
+
 export default function Tours() {
   const [Guests, setGuests] = useState([]);
   const [newGuest, setNewGuest] = useState({
@@ -15,6 +39,19 @@ export default function Tours() {
   });
   const [premiums, setPremiums] = useState([]);
   const navigate = useNavigate();
+
+  // New state for editing
+  const [editGuestId, setEditGuestId] = useState(null);
+  const [editedGuest, setEditedGuest] = useState({
+    name: '',
+    owner: false,
+    non_owner: false,
+    gift: '',
+    pp: '',
+    tour_date: '',
+    notes: ''
+  });
+
 
   const fetchPremiums = async () => {
     try {
@@ -45,14 +82,8 @@ export default function Tours() {
 
   const addNewGuest = async () => {
     const updatedGuest = { ...newGuest };
-
     updatedGuest.owner = updatedGuest.owner ? 'Yes' : 'No';
     updatedGuest.non_owner = updatedGuest.non_owner ? 'Yes' : 'No';
-
-    if (updatedGuest.owner === updatedGuest.non_owner) {
-      alert("Either 'Owner' or 'Non-Owner' must be checked, but not both or neither.");
-      return;
-    }
 
     try {
       const response = await fetch('http://localhost:4005/api/Guests', {
@@ -78,6 +109,42 @@ export default function Tours() {
       }
     } catch (error) {
       console.error(`Add new guest failed: ${error.message}`);
+    }
+  };
+  const startEditingGuest = (guest) => {
+    setEditGuestId(guest.guest_id);
+    const editedGuestData = { ...guest };
+    editedGuestData.owner = editedGuestData.owner === 'Yes' || editedGuestData.owner === true;
+    editedGuestData.non_owner = editedGuestData.non_owner === 'Yes' || editedGuestData.non_owner === true;
+    setEditedGuest(editedGuestData);
+  };
+  
+
+  const updateGuest = async () => {
+    try {
+      const response = await fetch(`http://localhost:4005/api/Guests/${editGuestId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(editedGuest),
+      });
+
+      if (response.ok) {
+        setGuests(Guests.map((guest) => (guest.guest_id === editGuestId ? editedGuest : guest)));
+        setEditGuestId(null);
+        setEditedGuest({
+          name: '',
+          owner: false,
+          non_owner: false,
+          gift: '',
+          pp: '',
+          tour_date: '',
+          notes: ''
+        });
+      }
+    } catch (error) {
+      console.error(`Update failed: ${error.message}`);
     }
   };
 
@@ -110,40 +177,43 @@ export default function Tours() {
       <h1>Tours</h1>
       <div>
         <h2>Add New Guest</h2>
-        <input type="text" placeholder="Name" value={newGuest.name} onChange={e => setNewGuest({ ...newGuest, name: e.target.value })} />
-        <label>
-          <input type="checkbox" checked={newGuest.owner} onChange={e => setNewGuest({ ...newGuest, owner: e.target.checked, non_owner: !e.target.checked })} />
-          Owner
-        </label>
-        <label>
-          <input type="checkbox" checked={newGuest.non_owner} onChange={e => setNewGuest({ ...newGuest, non_owner: e.target.checked, owner: !e.target.checked })} />
-          Non-Owner
-        </label>
-        <select value={newGuest.gift} onChange={e => setNewGuest({ ...newGuest, gift: e.target.value })}>
-          <option value="" disabled>Select gift</option>
-          {premiums.map((premiumObj, index) => (
-            <option key={index} value={premiumObj.premium}>{premiumObj.premium}</option>
-          ))}
-        </select>
-        <input type="text" placeholder="PP" value={newGuest.pp} onChange={e => setNewGuest({ ...newGuest, pp: e.target.value })} />
-        <input type="date" placeholder="Tour Date" value={newGuest.tour_date} onChange={e => setNewGuest({ ...newGuest, tour_date: e.target.value })} />
-        <input type="text" placeholder="Notes" value={newGuest.notes} onChange={e => setNewGuest({ ...newGuest, notes: e.target.value })} />
+        <CustomInput placeholder="Name" type="text" value={newGuest.name} onChange={e => setNewGuest({ ...newGuest, name: e.target.value })} />
+        <CustomCheckbox label="Owner" checked={newGuest.owner} onChange={e => setNewGuest({ ...newGuest, owner: e.target.checked, non_owner: !e.target.checked })} />
+        <CustomCheckbox label="Non-Owner" checked={newGuest.non_owner} onChange={e => setNewGuest({ ...newGuest, non_owner: e.target.checked, owner: !e.target.checked })} />
+        <CustomSelect value={newGuest.gift} options={premiums.map(premiumObj => premiumObj.premium)} onChange={e => setNewGuest({ ...newGuest, gift: e.target.value })} />
+        <CustomInput placeholder="PP" type="text" value={newGuest.pp} onChange={e => setNewGuest({ ...newGuest, pp: e.target.value })} />
+        <CustomInput placeholder="Tour Date" type="date" value={newGuest.tour_date} onChange={e => setNewGuest({ ...newGuest, tour_date: e.target.value })} />
+        <CustomInput placeholder="Notes" type="text" value={newGuest.notes} onChange={e => setNewGuest({ ...newGuest, notes: e.target.value })} />
         <button onClick={addNewGuest}>Add Guest</button>
       </div>
       <ul>
-        {Guests.map((guest, index) => (
-          <li key={index}>
-            <div>{guest.name}</div>
-            <div>Owner: {guest.owner}</div>
-            <div>Non-Owner: {guest.non_owner}</div>
-            <div>{guest.gift}</div>
-            <div>{guest.pp}</div>
-            <div>{guest.tour_date}</div>
-            <div>{guest.notes}</div>
-            <button onClick={() => deleteGuest(guest.guest_id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+  {Guests.map((guest, index) => (
+    <li key={index}>
+      <div>{guest.name}</div>
+      <div>Owner: {guest.owner}</div>
+      <div>Non-Owner: {guest.non_owner}</div>
+      <div>{guest.gift}</div>
+      <div>{guest.pp}</div>
+      <div>{guest.tour_date}</div>
+      <div>{guest.notes}</div>
+      {editGuestId === guest.guest_id ? (
+    <>
+      <CustomInput placeholder="Name" type="text" value={editedGuest.name} onChange={e => setEditedGuest({ ...editedGuest, name: e.target.value })} />
+      <CustomCheckbox label="Owner" checked={editedGuest.owner} onChange={e => setEditedGuest({ ...editedGuest, owner: e.target.checked, non_owner: !e.target.checked })} />
+      <CustomCheckbox label="Non-Owner" checked={editedGuest.non_owner} onChange={e => setEditedGuest({ ...editedGuest, non_owner: e.target.checked, owner: !e.target.checked })} />
+      <CustomSelect value={editedGuest.gift} options={premiums.map(premiumObj => premiumObj.premium)} onChange={e => setEditedGuest({ ...editedGuest, gift: e.target.value })} />
+      <CustomInput placeholder="PP" type="text" value={editedGuest.pp} onChange={e => setEditedGuest({ ...editedGuest, pp: e.target.value })} />
+      <CustomInput placeholder="Tour Date" type="date" value={editedGuest.tour_date} onChange={e => setEditedGuest({ ...editedGuest, tour_date: e.target.value })} />
+      <CustomInput placeholder="Notes" type="text" value={editedGuest.notes} onChange={e => setEditedGuest({ ...editedGuest, notes: e.target.value })} />
+      <button onClick={updateGuest}>Save</button>
+    </>
+  ) : (
+    <button onClick={() => startEditingGuest(guest)}>Edit</button>
+  )}
+    <button onClick={() => deleteGuest(guest.guest_id)}>Delete</button>
+    </li>
+  ))}
+</ul>
     </div>
   );
 }
