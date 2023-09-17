f3d740a2c2b86da18eb5018876bf7506edafd9af
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-09-16 20:44:45 -0700

    added css to navbar and collapse functionality while on smaller window

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,39 @@
+import React from 'react';
 import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './navbar.css';
 
 function Navbar() {
- return (
-  <div className="Navbar">
-    <header>
-     <ul>
-      <li>
-       <Link to="/">Home</Link>
-      </li>
-      <li>
-       <Link to="/premiums">Premiums</Link>
-      </li>
-      <li>
-       <Link to="/tours">Tours</Link>
-      </li>
-      <li>
-       <Link to="/login">Login</Link>
-      </li>
-      <li>
-       <Link to="/logout">Logout</Link>
-      </li>
-     </ul>
-    </header>
-  </div>
- );
+  return (
+    <div className="Navbar">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/">TourHub</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/premiums">Premiums</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/tours">Tours</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">Login</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/logout">Logout</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
