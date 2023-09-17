113b26491adab5438555365626d9b5329c46152d
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-09-16 23:13:28 -0700

    completed Navbar.jsx/navbar.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,30 +4,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './navbar.css';
 
+
 function Navbar() {
   return (
-    <div className="Navbar">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">TourHub</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <div className="Navbar supabase-nav">
+      <nav className="navbar navbar-expand-lg">
+        <a className="navbar-brand compressible" href="/">TourHub</a>
+        <button className="navbar-toggler custom-toggler compressible" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <Link className="nav-link" to="/">Home</Link>
+              <Link className="nav-link compressible" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/premiums">Premiums</Link>
+              <Link className="nav-link compressible" to="/premiums">Premiums</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/tours">Tours</Link>
+              <Link className="nav-link compressible" to="/tours">Tours</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/login">Login</Link>
+              <Link className="nav-link compressible" to="/login">Login</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/logout">Logout</Link>
+              <Link className="nav-link compressible" to="/logout">Logout</Link>
             </li>
           </ul>
         </div>
