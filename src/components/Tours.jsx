119cadcc16a2627a332933f18ab3b6395244b6ce
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-09-18 09:58:13 -0700

    added color for jsx save button

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import supabase from '../services/supabaseClient';
 import ProjectedPayCalculator from './ProjectedPayCalculator';
+import "../styles/tours.css"
 
 function CustomInput({ placeholder, type, value, onChange }) {
   return <input type={type} placeholder={placeholder} value={value || ''} onChange={onChange} />;
@@ -160,7 +161,7 @@ export default function Tours() {
 
 
   const renderGuestForm = (guest, setGuestFunction, submitFunction, buttonText) => (
-    <>
+    <form className="custom-form">
       <CustomInput placeholder="Name" type="text" value={guest.name} onChange={e => setGuestFunction({ ...guest, name: e.target.value })} />
       <CustomCheckbox label="Owner" checked={guest.owner} onChange={e => setGuestFunction({ ...guest, owner: e.target.checked, non_owner: !e.target.checked })} />
       <CustomCheckbox label="Non-Owner" checked={guest.non_owner} onChange={e => setGuestFunction({ ...guest, non_owner: e.target.checked, owner: !e.target.checked })} />
@@ -169,8 +170,14 @@ export default function Tours() {
       <CustomInput placeholder="Projected Pay" type="smallint" value={guest.projected_pay} onChange={e => setGuestFunction({ ...guest, projected_pay: e.target.value })} />
       <CustomInput placeholder="Tour Date" type="date" value={guest.tour_date} onChange={e => setGuestFunction({ ...guest, tour_date: e.target.value })} />
       <CustomInput placeholder="Notes" type="text" value={guest.notes} onChange={e => setGuestFunction({ ...guest, notes: e.target.value })} />
-      <button onClick={submitFunction}>{buttonText}</button>
-    </>
+      {buttonText === "Save" ? (
+      <button className="save-button" type="button" onClick={submitFunction}>
+        {buttonText}
+      </button>
+    ) : (
+      <button type="button" onClick={submitFunction}>{buttonText}</button>
+    )}
+    </form>
   );
 
   useEffect(() => {
@@ -198,15 +205,15 @@ export default function Tours() {
   }, [navigate]);
 
   return (
-    <div>
-      <h1>Tours</h1>
-      <div>
+    <div className="tours-container">
+      <h1 className="tours-title">Tours</h1>
+      <div className="add-new-guest">
         <h2>Add New Guest</h2>
         {renderGuestForm(newGuest, setNewGuest, addNewGuest, "Add Guest")}
       </div>
-      <ul>
+      <ul className="guest-list">
         {Guests.map((guest, index) => (
-          <li key={index}>
+          <li key={index} className="guest-item">
       <div>{guest.name}</div>
       <div>Owner: {guest.owner}</div>
       <div>Non-Owner: {guest.non_owner}</div>
@@ -215,12 +222,14 @@ export default function Tours() {
       <div>{guest.projected_pay}</div>
       <div>{guest.tour_date}</div>
       <div>{guest.notes}</div>
-      {editGuestId === guest.guest_id ? (
-              renderGuestForm(editedGuest, setEditedGuest, updateGuest, "Save")
-            ) : (
-              <button onClick={() => startEditingGuest(guest)}>Edit</button>
-            )}
-            <button onClick={() => deleteGuest(guest.guest_id)}>Delete</button>
+      <div className="guest-actions">
+        {editGuestId === guest.guest_id ? (
+          renderGuestForm(editedGuest, setEditedGuest, updateGuest, "Save")
+        ) : (
+          <button className="edit-button" onClick={() => startEditingGuest(guest)}>Edit</button>
+        )}
+              <button className="delete-button" onClick={() => deleteGuest(guest.guest_id)}>Delete</button>
+            </div>
           </li>
         ))}
       </ul>
