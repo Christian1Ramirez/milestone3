d75446ff8168abe097059dfaea90cc034d708359
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-09-24 09:40:29 -0700

    REACT_APP_ANON_KEY FIGURED OUT

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://aycixvbjldecjoybxpih.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5Y2l4dmJqbGRlY2pveWJ4cGloIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQyNzMzMjksImV4cCI6MjAwOTg0OTMyOX0.oxV3mb756Qccbf3IoUFVoPhx3s78tq1-w3Oe3yRfvHY'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = process.env.REACT_APP_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
